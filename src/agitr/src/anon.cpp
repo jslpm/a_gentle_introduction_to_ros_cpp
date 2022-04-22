8f5d517e54b86c7bff3fa05ca5b12fe63bfc4638
Jose Pascal <jose.pascal@gmail.com>
2022-04-22 13:39:54 -0400

    Add anonymous node example file

---
+++ src/agitr/src/anon.cpp
@@ -0,0 +1,18 @@
+// This program starts with an anonymous name, which
+// allow mutiple copies to execute at the same time,
+// without meeding to manually create distinct names
+// for each of them.
+
+#include <ros/ros.h>
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "anon", ros::init_options::AnonymousName);
+    ros::NodeHandle nh;
+    
+    ros::Rate rate(1);
+    
+    while(ros::ok()) {
+        ROS_INFO_STREAM("This message is from "<< ros::this_node::getName());
+        rate.sleep();
+    }
+}
